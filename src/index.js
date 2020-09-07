ab3568595310a1752e8a3b89ca49d6851380e51e
Enrique Sanchez <enrike.sanchez@gmail.com>
2020-09-06 19:44:51 -0600

    Added serve dependency

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './containers/App/App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
