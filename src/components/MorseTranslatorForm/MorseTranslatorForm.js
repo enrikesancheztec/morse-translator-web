324eb58c10af2ee6ddab9b93cb5c4f6c800a8233
Enrique Sanchez <enrike.sanchez@gmail.com>
2020-09-06 21:28:03 -0600

    Added error handling

--- src/components/MorseTranslatorForm/MorseTranslatorForm.js
+++ src/components/MorseTranslatorForm/MorseTranslatorForm.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Alert } from "react-bootstrap";
 
 class MorseTranslatorForm extends Component {
     render() {
+        const errorMessage = this.props.errorMessage;
+        var alert = '';
+
+        if (errorMessage !== "") {
+            alert = <Alert variant='danger'> { this.props.errorMessage } </Alert>;
+        }
+
         return (
             <Form onSubmit={this.props.onSubmit}>
+                {alert}
+
                 <Form.Group controlId="message">
                     <Form.Label>Message to translate</Form.Label>
                     <Form.Control type="text" placeholder="Enter a message" value={this.props.message} onChange={this.props.onMessageChange} />
--- src/containers/MorseTranslator/MorseTranslator.js
+++ src/containers/MorseTranslator/MorseTranslator.js
@@ -8,30 +8,31 @@ class MorseTranslator extends Component {
         super(props);
 
         this.state = {
-            message : '',
-            history : [],
-            counter : 0
+            message: '',
+            history: [],
+            counter: 0,
+            errorMessage: ''
         };
 
         this.handleMessageChange = this.handleMessageChange.bind(this);
         this.handleMessageSubmit = this.handleMessageSubmit.bind(this);
-    }    
+    }
 
     render() {
         return (
             <div>
                 <h1>Morse Code Online Translator</h1>
-                <MorseTranslatorForm message={this.state.message} onMessageChange={this.handleMessageChange} onSubmit={this.handleMessageSubmit} />
+                <MorseTranslatorForm message={this.state.message} errorMessage={this.state.errorMessage} onMessageChange={this.handleMessageChange} onSubmit={this.handleMessageSubmit} />
                 <br />
                 <MorseTranslatorHistoryTable history={this.state.history} />
             </div>
-        )
+        );
     }
 
     handleMessageChange(event) {
         this.setState({ message: event.target.value });
         event.preventDefault();
-    }    
+    }
 
     handleMessageSubmit(event) {
         var data = {
@@ -39,19 +40,24 @@ class MorseTranslator extends Component {
         }
 
         MorseTranslatorService.postMessage(data)
-        .then(res => {
-          const result = res.data;
-          var newHistory = this.state.history;
-          var newCounter = this.state.counter + 1;
-          result.counter = newCounter;
-          newHistory.push(result);
-          this.setState({ history : newHistory, counter : newCounter });
-        })      
+            .then(res => {
+                const result = res.data;
+                var newHistory = this.state.history;
+                var newCounter = this.state.counter + 1;
+                result.counter = newCounter;
+                newHistory.push(result);
+                this.setState({ history: newHistory, counter: newCounter });
+            }).catch(err => {
+                if (err.response.status === 400) {
+                    let errorMessage = err.response.data.error.errors[0].msg;
+                    this.setState({ errorMessage: errorMessage });
+                }
+            })
 
-        this.setState({ message : '' });
+        this.setState({ message: '', errorMessage: '' });
 
         event.preventDefault();
-    }     
+    }
 }
 
 export default MorseTranslator;
